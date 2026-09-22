657cda169a1a3ae31b454c5a912d4821f243b990
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-22 13:25:28 +0900

    Add removeFromArray function

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,12 +1,6 @@
 const removeFromArray = function(array, ...words) {
     // ...word:残余引数、配列で受け取れる
-    let removedArray = [];
-    for (const item of array) {
-        if(!words.includes(item)) {
-            removedArray.push(item);
-        }
-    }
-    return removedArray;
+    return array.filter((item) => !words.includes(item))
 };
 
 // Do not edit below this line
