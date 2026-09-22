07cc9e68da81cc9c6484963acae016811af0a076
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-22 14:44:36 +0900

    Add tempConversion function

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) { //華氏から摂氏
+  return Math.round((5 / 9) * (fahrenheit - 32) * 10) / 10; //小数第一位で四捨五入
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) { //摂氏から華氏
+  return Math.round(((9 / 5) * celsius + 32) * 10) / 10; //小数第一位で四捨五入
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
