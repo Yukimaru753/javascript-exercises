8530849a2baa991480c5bae33ca0c347586e3d75
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-22 14:20:49 +0900

    Refactor leapYears for raedability

--- foundations/loops_and_arrays/05_leapYears/leapYears.js
+++ foundations/loops_and_arrays/05_leapYears/leapYears.js
@@ -1,13 +1,17 @@
-const leapYears = function(year) {
-    if(year % 400 === 0) {
-        return true;
-    } else if (year % 100 === 0) {
-        return false;
-    } else if (year % 4 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+const leapYears = function (year) {
+  const isYearDivisibleByFourHundred = year % 400 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFour = year % 4 === 0;
+
+  if (isYearDivisibleByFourHundred) { //400でわれるならうるう年
+    return true;
+  } else if (isCentury) { //100でわれるのはうるう年ではない
+    return false;
+  } else if (isYearDivisibleByFour) { //4でわれるならうるう年
+    return true;
+  } else { //残りはうるう年ではない
+    return false;
+  }
 };
 
 // Do not edit below this line
