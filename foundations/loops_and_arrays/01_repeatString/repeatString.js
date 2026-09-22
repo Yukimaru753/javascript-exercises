d56361af0e20e22ff70053b62afe61f55c2c348e
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-22 12:41:38 +0900

    Add repeatString function

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let repeat = "";
+  if (num > 0) {
+    for (let i = 0; i < num; i++) {
+      repeat += string;
+    }
+  } else if (num < 0) {
+    repeat = "ERROR";
+  }
 
+  return repeat;
 };
 
 // Do not edit below this line
