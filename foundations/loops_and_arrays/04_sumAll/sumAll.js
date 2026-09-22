573c3defe2da076fce8e0144ff4c59fa90a4ced3
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-22 13:48:29 +0900

    Add sumAll function

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (
+    typeof min !== "number" ||
+    typeof max !== "number" ||
+    min < 0 ||
+    max < 0 ||
+    !Number.isInteger(min) ||
+    !Number.isInteger(max)
+  ) {
+    return "ERROR";
+  } else if (min > max) {
+    let passNum = max;
+    max = min;
+    min = passNum;
+  }
 
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
